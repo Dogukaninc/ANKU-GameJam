6bbd0530559b16505caf424cc24db43d2ad2b455
Doğukan <d_incirbolen@hotmail.com>
2024-05-11 01:13:54 +0300

    Player Controller
    Player Controller

---
+++ ANKUGameJam/Assets/Game Files/Scripts/PuzzleGame_1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleGame_1 : MonoBehaviour, IInteractable
+{
+    public void Interaction()
+    {
+        Debug.Log("Puzzle Etkileþime Girildi!!!");
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+}
